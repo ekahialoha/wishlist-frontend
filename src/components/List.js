8c80964a24c03b74fa990eb46df372f0d6539255
Molly Stone <molly.stone89@gmail.com>
2019-06-09 17:07:48 -0400

    Create delete list request

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Item from './Item';
+import { Redirect } from 'react-router-dom';
 
 class List extends Component {
     constructor(props) {
@@ -7,7 +8,9 @@ class List extends Component {
         this.state = {
             list: {
                 items: []
-            }
+            },
+            random: false,
+            redirect: false
         };
     }
 
@@ -16,13 +19,15 @@ class List extends Component {
     }
 
     fetchList = () => {
-        console.log('fetch');
         let url = 'http://localhost:3000/lists/';
         let path = this.props.location.pathname;
         if (path === '/') {
             url += 'random';
+            this.setState({
+                random: true
+            })
         } else {
-            const id = path[path.length - 1];
+            const id = path.split('/')[2];
             url += `${id}`;
         }
         fetch(url)
@@ -65,26 +70,44 @@ class List extends Component {
         });
     }
 
+    handleDelete = () => {
+        fetch(`http://localhost:3000/lists/${this.state.list.id}`, {
+            method: 'DELETE'
+        }).then(data => {
+            console.log('deleted');
+        }).catch(err => console.log('delete item error: ', err));
+        this.setState({
+            redirect: true
+        })
+    }
+
     render() {
-        // console.log(this.state.list);
+        if (this.state.redirect === true) {
+            return <Redirect to='/' />
+        }
         return (
             <div>
                 <div>
-                    <h2>Featured Wishlist</h2>
+                    {this.state.random ? <h2>Featured Wishlist</h2> : '' }
                     <h3>{this.state.list.name}</h3>
+                    <i className="fas fa-edit"></i>
+                    <i className="far fa-trash-alt" onClick={this.handleDelete}></i>
                 </div>
-                <div className="itemList">
-                    {this.state.list.items.map((item, index) => {
-                        return (
-                            <Item
-                                key={index}
-                                index={index}
-                                item={item}
-                                handlePurchaser={this.handlePurchaser}
-                            />
-                        );
-                    })}
-                </div>
+                {this.state.list.items ?
+                    <div className="itemList">
+                        {this.state.list.items.map((item, index) => {
+                            return (
+                                <Item
+                                    key={index}
+                                    index={index}
+                                    item={item}
+                                    handlePurchaser={this.handlePurchaser}
+                                />
+                            );
+                        })}
+                    </div> : ''
+                }
+
             </div>
         );
     }
