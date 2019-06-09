267d493fecf73f1b580d329de0731e3c4b0ff68e
Molly Stone <molly.stone89@gmail.com>
2019-06-09 11:52:01 -0400

    Create view list component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Media from 'react-media';
 import Navigation from './components/Navigation';
 import Search from './components/Search';
 import Index from './components/Index';
+import List from './components/List';
 import CreateList from './components/CreateList';
 
 class App extends Component {
@@ -19,7 +20,7 @@ class App extends Component {
                         <Route exact path='/' component={Index} /> // Random Index
                         <Route path='/new' component={CreateList} /> // Create List
                         <Redirect from='/test' to='/about' />
-                        <Route path='/view/:id' component={() => <h2>View List</h2>} /> // View List
+                        <Route path='/view/:id' component={List} /> // View List
                         <Route path='/search' component={SearchResults}/> // Search Results
                     </Switch>
                 </Router>
---
+++ src/components/List.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import Item from './Item';
+
+class List extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: {},
+            items: []
+        };
+    }
+
+    componentDidMount = () => {
+        this.fetchList(1) // ID FOR TESTING
+    }
+
+    fetchList = (id) => {
+        fetch(`http://localhost:3000/lists/${id}`)
+            .then(foundList => foundList.json())
+            .then(jsonData => this.setList(jsonData))
+    }
+
+    setList = (list) => {
+        const items = list.items.slice();
+        this.setState({
+            list: list,
+            items: items
+        });
+        console.log(this.state);
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <h2>{this.state.list.name}'s Wishlist</h2>
+                </div>
+                <div className="itemList">
+                    {this.state.items.map((item, index) => {
+                        return (
+                            <Item
+                                key={index}
+                                index={index}
+                                item={item}
+                            />
+                        )
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default List;
+
+// {this.state.list.items.map((item, index) => {
+//     return (
+//         <Item
+//             key={index}
+//             index={index}
+//             item={item}
+//             handlePurchaser={this.handlePurchaser}
+//         />
+//     );
+// })}
