7951135a91ebc154fd90f49a76762e289b117fa5
Chris <chris@ekahi.co>
2019-06-09 15:20:41 -1000

    Add Navbar.Text/Navbar collapsed on all sizes now

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -19,14 +19,16 @@ class Navigation extends Component {
 
     render() {
         return (
-            <Navbar bg="dark" variant="dark" expand="lg" expanded={this.state.expanded} onToggle={this.handleNavToggle}>
+            <Navbar bg="dark" variant="dark" expand="false" expanded={this.state.expanded} onToggle={this.handleNavToggle}>
                 <Navbar.Brand>Get Started</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav>
                         <Search handleNavToggle={this.handleNavToggle} />
                         <Link to="/new" onClick={() => this.handleNavToggle(false)}><Button>New WishList</Button></Link>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet nisl non tellus elementum consequat. Nunc est libero, semper sit amet ante sit amet, dapibus pellentesque nibh. Aliquam vehicula sollicitudin felis, eu convallis magna pellentesque non. Integer consequat facilisis mauris et venenatis. Vestibulum nisl ex, vulputate id mi aliquet, fringilla tristique purus. Praesent quis magna mollis, hendrerit neque lacinia, cursus justo. Morbi non diam risus. Nullam odio orci, finibus a vulputate nec, blandit nec lectus.</p>
+                        <Navbar.Text>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet nisl non tellus elementum consequat. Nunc est libero, semper sit amet ante sit amet, dapibus pellentesque nibh. Aliquam vehicula sollicitudin felis, eu convallis magna pellentesque non. Integer consequat facilisis mauris et venenatis. Vestibulum nisl ex, vulputate id mi aliquet, fringilla tristique purus. Praesent quis magna mollis, hendrerit neque lacinia, cursus justo. Morbi non diam risus. Nullam odio orci, finibus a vulputate nec, blandit nec lectus.
+                        </Navbar.Text>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
