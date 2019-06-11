93c28a9b0273130aa89e8bc43c285e2928bc95f2
Molly Stone <molly.stone89@gmail.com>
2019-06-11 12:09:39 -0400

    Update desktop view of navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Search from './Search';
-import { Navbar, Nav, Button, Collapse } from 'react-bootstrap';
+import { Navbar, Nav, Button, Collapse, NavDropdown } from 'react-bootstrap';
+import Media from 'react-media';
 
 class Navigation extends Component {
     constructor(props) {
@@ -27,39 +28,73 @@ class Navigation extends Component {
 
     render() {
         return (
-            <Navbar
-                expand="false"
-                expanded={this.state.expanded}
-                onToggle={this.handleNavToggle}
-            >
-                <Navbar.Brand>Get Started</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav>
-                        <Navbar.Text><Search handleNavToggle={this.handleNavToggle} /></Navbar.Text>
-                        <Navbar.Text>
-                            <Link to="/new">
-                                <Button
-                                    block
-                                    size="lg"
-                                    className="new-list-btn"
-                                    onClick={() => this.handleNavToggle(false)}
-                                >
-                                    Create WishList
-                                </Button>
-                            </Link>
-                        </Navbar.Text>
-                        <Navbar.Text>
-                            <Button block onClick={this.toggleAbout}>About</Button>
-                        </Navbar.Text>
-                            <Collapse in={this.state.aboutOpen}>
-                                <Navbar.Text className="about">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet nisl non tellus elementum consequat. Nunc est libero, semper sit amet ante sit amet, dapibus pellentesque nibh. Aliquam vehicula sollicitudin felis, eu convallis magna pellentesque non. Integer consequat facilisis mauris et venenatis.
+            <div>
+                <Media query="(max-width: 996px)">
+                    <Navbar
+                        expand="false"
+                        expanded={this.state.expanded}
+                        onToggle={this.handleNavToggle}
+                    >
+                        <Navbar.Brand>Get Started</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav>
+                                <Navbar.Text><Search handleNavToggle={this.handleNavToggle} /></Navbar.Text>
+                                <Navbar.Text>
+                                    <Link to="/new">
+                                        <Button
+                                            block
+                                            size="lg"
+                                            className="new-list-btn"
+                                            onClick={() => this.handleNavToggle(false)}
+                                        >
+                                            Create WishList
+                                        </Button>
+                                    </Link>
                                 </Navbar.Text>
-                            </Collapse>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+                                <Navbar.Text>
+                                    <Button block onClick={this.toggleAbout}>About</Button>
+                                </Navbar.Text>
+                                    <Collapse in={this.state.aboutOpen}>
+                                        <Navbar.Text className="about">
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet nisl non tellus elementum consequat. Nunc est libero, semper sit amet ante sit amet, dapibus pellentesque nibh. Aliquam vehicula sollicitudin felis, eu convallis magna pellentesque non. Integer consequat facilisis mauris et venenatis.
+                                        </Navbar.Text>
+                                    </Collapse>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </Media>
+                <Media query="(min-width: 997px)">
+                    <Navbar
+                        expand="lg"
+                        expanded={this.state.expanded}
+                        onToggle={this.handleNavToggle}
+                    >
+                        <Navbar.Brand>Get Started</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav>
+                                <Navbar.Text>
+                                    <Link to="/new">
+                                        <Button
+                                            size="lg"
+                                            className="desktop-new-list-btn"
+                                            onClick={() => this.handleNavToggle(false)}
+                                        >
+                                            Create WishList
+                                        </Button>
+                                    </Link>
+                                </Navbar.Text>
+                                <NavDropdown className="desktop-creators" title="Creators">
+                                    <NavDropdown.Item>Christian</NavDropdown.Item>
+                                    <NavDropdown.Item>Molly</NavDropdown.Item>
+                                </NavDropdown>
+                                <Navbar.Text className="desktop-searchbar"><Search handleNavToggle={this.handleNavToggle} /></Navbar.Text>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </Media>
+            </div>
         );
     }
 }
